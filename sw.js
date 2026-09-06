fa7a19f6f1ae55f8141d69d765195156131a3c16
Atomic.Singer EAP <181106935+AlexTardisHbn@users.noreply.github.com>
2026-09-06 20:00:01 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'elvis-site-v1';
+const CORE_ASSETS = [
+  'index.html',
+  'galerie.html',
+  'icon-192.png',
+  'icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Stratégie "réseau d'abord, cache en secours" : privilégie le contenu à jour
+// (avis/articles, statistiques) mais garde le site consultable hors-ligne.
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
